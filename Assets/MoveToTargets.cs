0f9cc2ce2bcb8173c2ec5f179ef2752209486e73
Techtide <greenie@Armans-MBP.local>
2017-11-19 13:44:10 +0000

    Added project- 1.44 pm

---
+++ Assets/MoveToTargets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTargets : MonoBehaviour
+{
+
+	public GameObject targetsParent;
+	private Transform[] targets;
+	public int currentIndex;
+
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		targets = targetsParent.GetComponentsInChildren<Transform>();
+		currentIndex = Random.Range(0, targets.Length);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (agent.remainingDistance < 2)
+		{
+			/*currentIndex++;
+			if (currentIndex >= targets.Length)
+			{
+				currentIndex = 0;
+			}*/
+			currentIndex = Random.Range(0, targets.Length);
+			agent.SetDestination(targets[currentIndex].position);
+		}
+	}
+}
---
+++ Assets/PlayerBehaviourDeterminer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerBehaviourDeterminer : MonoBehaviour
+{
+	private String[] behavioursArray = 
+		{"Headphones", "Pocket Knives", "Katana", "Glasses"/*, "Textbook", "Magazine"*/};
+
+	public GameObject[] accesories;
+	
+	private String personsBehaviour;
+	// Use this for initialization
+	void Start ()
+	{
+		/*int randomIndex = UnityEngine.Random.Range(0, behavioursArray.Length + 1);
+		personsBehaviour = randomIndex == behavioursArray.Length ? "Nothing" : behavioursArray[randomIndex];*/
+
+		for (int i = 0; i < accesories.Length; i++) accesories[i].active = UnityEngine.Random.Range(0, 4) == 0;
+		
+		/*switch (personsBehaviour)
+		{
+				case "Headphones":
+					accesories[0].active = true;
+					accesories[1].active = false;
+					accesories[2].active = false;
+					accesories[3].active = false;
+					//accesories[4].active = false;
+					//accesories[5].active = false;
+					break;
+				case "Pocket Knives":
+					accesories[0].active = false;
+					accesories[1].active = true;
+					accesories[2].active = false;
+					accesories[3].active = false;
+					//accesories[4].active = false;
+					//accesories[5].active = false;
+					break;
+				case "Katana":
+					accesories[0].active = false;
+					accesories[1].active = false;
+					accesories[2].active = true;
+					accesories[3].active = false;
+					//accesories[4].active = false;
+					//accesories[5].active = false;
+					break;
+				case "Glasses":
+					accesories[0].active = false;
+					accesories[1].active = false;
+					accesories[2].active = false;
+					accesories[3].active = true;
+					/*accesories[4].active = false;
+					accesories[5].active = false; #1#
+					break;
+				/*case "Textbook":
+					accesories[0].active = false;
+					accesories[1].active = false;
+					accesories[2].active = false;
+					accesories[3].active = false;
+					accesories[4].active = true;
+					accesories[5].active = false;
+					break;
+				case "Magazine":
+					accesories[0].active = false;
+					accesories[1].active = false;
+					accesories[2].active = false;
+					accesories[3].active = false;
+					accesories[4].active = false;
+					accesories[5].active = true;
+					break;#1#
+			
+		}*/
+	}
+}
